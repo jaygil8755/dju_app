f9c36cbe1ca536685d7af48a6402cab798cbd575
jaygil8755 <68841210+jaygil8755@users.noreply.github.com>
2024-02-02 16:50:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## 이미지 배경 제거 앱")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/jaygil8755/dju
+    _app) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
 st.sidebar.write("## 업로드하고 다운로드 받기 :gear:")
 
